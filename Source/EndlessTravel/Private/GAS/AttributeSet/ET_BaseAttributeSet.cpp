3d0e68b4eddffe9f14119ed1a54e5a2f77067d26
Olegnormchel83 <cranberries2000@gmail.com>
2025-03-10 17:21:46 +0300

    Created ShootAbility (not final)

--- Source/EndlessTravel/Private/GAS/AttributeSet/ET_BaseAttributeSet.cpp
+++ Source/EndlessTravel/Private/GAS/AttributeSet/ET_BaseAttributeSet.cpp
@@ -11,29 +11,88 @@ void UET_BaseAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCal
 {
 	Super::PostGameplayEffectExecute(Data);
 
+	//Health
 	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
 	{
 		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
 	}
 
+	//Stamine
 	if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
 	{
 		SetStamina(FMath::Clamp(GetStamina(), 0.f, GetMaxStamina()));
 	}
 
+	//Mana
 	if (Data.EvaluatedData.Attribute == GetManaAttribute())
 	{
 		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
 	}
 
+	//Armor
+	if (Data.EvaluatedData.Attribute == GetArmorAttribute())
+	{
+		SetArmor(GetArmor());
+	}
+
+	//Evasion
+	if (Data.EvaluatedData.Attribute == GetEvasionAttribute())
+	{
+		SetEvasion(GetEvasion());
+	}
+
+	//StealthDuration
 	if (Data.EvaluatedData.Attribute == GetStealthDurationAttribute())
 	{
 		SetStealthDuration(GetStealthDuration());
 	}
 
+	//StealthPower
 	if (Data.EvaluatedData.Attribute == GetStealthPowerAttribute())
 	{
 		SetStealthPower(GetStealthPower());
+	}
+
+	//CriticalChance
+	if (Data.EvaluatedData.Attribute == GetCriticalChanceAttribute())
+	{
+		SetCriticalChance(GetCriticalChance());
+	}
+
+	//CriticalDamage
+	if (Data.EvaluatedData.Attribute == GetCriticalDamageAttribute())
+	{
+		SetCriticalDamage(GetCriticalDamage());
+	}
+
+	//WeaponDamage
+	if (Data.EvaluatedData.Attribute == GetWeaponDamageAttribute())
+	{
+		SetWeaponDamage(GetWeaponDamage());
+	}
+
+	//WeaponDistance
+	if (Data.EvaluatedData.Attribute == GetWeaponDistanceAttribute())
+	{
+		SetWeaponDistance(GetWeaponDistance());
+	}
+
+	//ShellCount
+	if (Data.EvaluatedData.Attribute == GetShellCountAttribute())
+	{
+		SetShellCount(GetShellCount());
+	}
+
+	//FireRate
+	if (Data.EvaluatedData.Attribute == GetFireRateAttribute())
+	{
+		SetFireRate(GetFireRate());
+	}
+
+	//Patrons
+	if (Data.EvaluatedData.Attribute == GetCurrentPatronsCountAttribute())
+	{
+		SetCurrentPatronsCount(FMath::Clamp(GetCurrentPatronsCount(), 0.f, GetMaxPatronsCount()));
 	}
 }
 
--- Source/EndlessTravel/Public/GAS/AttributeSet/ET_BaseAttributeSet.h
+++ Source/EndlessTravel/Public/GAS/AttributeSet/ET_BaseAttributeSet.h
@@ -21,50 +21,91 @@ class ENDLESSTRAVEL_API UET_BaseAttributeSet : public UAttributeSet
 
 public:
 	//Health
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Health", 
-		meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Health")
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, Health)
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Health", 
-		meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Health")
 	FGameplayAttributeData MaxHealth;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, MaxHealth)
 
 	//Mana
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Mana", 
-		meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Mana")
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, Mana)
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Mana", 
-		meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Mana")
 	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, MaxMana)
 
 	//Stamina
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stamina", 
-			meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stamina")
 	FGameplayAttributeData Stamina;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, Stamina)
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stamina", 
-		meta = (ClampMin = "0", UIMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stamina")
 	FGameplayAttributeData MaxStamina;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, MaxStamina)
 
+	//Armor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Armor")
+	FGameplayAttributeData Armor;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, Armor)
+
+	//Evasion
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Evasion")
+	FGameplayAttributeData Evasion;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, Evasion)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Evasion")
+	FGameplayAttributeData MaxEvasion;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, MaxEvasion)
+
 	//Stealth
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stealth", 
-			meta = (ClampMin = "5", UIMin = "5"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stealth")
 	FGameplayAttributeData StealthDuration;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, StealthDuration)
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stealth", 
-			meta = (ClampMin = "0", ClampMax = "1", UIMin = "0", UIMax = "1"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Stealth")
 	FGameplayAttributeData StealthPower;
 	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, StealthPower)
 
-		//Functions
+	//Combat
+	//Combat|Crit
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Crit")
+	FGameplayAttributeData CriticalChance;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, CriticalChance)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Crit")
+	FGameplayAttributeData CriticalDamage;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, CriticalDamage)
+
+	//Combat|Weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData WeaponDamage;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, WeaponDamage)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData WeaponDistance;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, WeaponDistance)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData ShellCount;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, ShellCount)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData FireRate;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, FireRate)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData CurrentPatronsCount;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, CurrentPatronsCount)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes|Combat|Weapon")
+	FGameplayAttributeData MaxPatronsCount;
+	ATTRIBUTE_ACCESSORS(UET_BaseAttributeSet, MaxPatronsCount)
+
+	//Functions
 		virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 		//virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
